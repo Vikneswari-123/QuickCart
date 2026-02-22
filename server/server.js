8d270695ff36cbcd5cca4fc652abd4940e734d80
Vikneswari-123 <vikneswarimukunthan@gmail.com>
2026-02-22 19:31:49 +0530

    Fix correct frontend URL in CORS

--- server/server.js
+++ server/server.js
@@ -18,14 +18,18 @@ const port = process.env.PORT || 4000;
 await connectDB()
 await connectCloudinary()
 
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = ['http://localhost:5173',
+   process.env.FRONTEND_URL
+]
 
 app.post('/stripe', express.raw({type: 'application/json'}), stripeWebhooks)
 
 // Middleware configuration
+app.use(cors({origin: ["http://localhost:5173",
+    "https://frontend-quickcart.vercel.app"], credentials: true}));
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({origin: allowedOrigins, credentials: true}));
+
 
 app.get('/', (req, res)=> res.send("API is Working"));
 
